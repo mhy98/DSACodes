025db8db270461c752339a396225685cc07bcd5d
Silence <945001357@qq.com>
2019-11-30 21:40:30 +0800

    add exp1

--- 曹嘉祺/cal/init.ml
+++
@@ -1,3 +0,0 @@
-(def ssPoly (arg POLY p FLOAT a FLOAT b) (multi (let ps (sPoly p)) (let aa (calPoly ps a)) (let bb (calPoly ps b)) (print (sub bb aa)) (br) (delElem ps) (delElem aa) (delElem bb)))
-(def printToZero (arg INT a) (multi (let tmp a) (for (bigger tmp 0) (multi (print tmp) (br) (let tmp (sub tmp 1)))) (delElem tmp)))
-#end
